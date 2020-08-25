044a7d7b4fd81d6a090136ff6ee943602eb9a5a6
Jeehoon Son <Humanoiid@users.noreply.github.com>
2020-08-25 16:43:45 +0900

    register control + time control
    make new ino file

--- src/ARDUINO_STSPIN220_Stepper_manual/ARDUINO_STSPIN220_Stepper_manual.ino
+++ src/ARDUINO_STSPIN220_Stepper_manual/ARDUINO_STSPIN220_Stepper_manual.ino
@@ -2,6 +2,15 @@
  *  
  *  1. ustepping mode: https://www.pololu.com/product/2876
  *  2. audio freq noise & acc/decelation: https://blog.naver.com/motor_bank/221591338372
+ *  3. time control: http://rokitarduino.org/2014/05/23/input%EA%B3%BC-input_pullup%EC%9D%98-%EC%B0%A8%EC%9D%B4/
+ *  4. register GPIO: http://blog.naver.com/PostView.nhn?blogId=twophase&logNo=221004945877
+ *  5. register, gpio, pin informations: https://opentutorials.org/module/1837/10642
+ *  
+ *  
+ *  * micro() function takes around 4 microseconds. 
+ *  as I need to control GPIO control around 4 microseconds, use GPIO register & minimum micro() to control time
+ *  
+ *  * Consider 
  *  
  */
 #include <Stepper.h>
@@ -9,13 +18,13 @@
 #include <MsTimer2.h> // timer for Acceleration/deceleration control
 
 
-int mode_1 = 5; // CN9 pin6 /Arduino 5
-int mode_2 = 10; // CN5 pin3 /Arduino 10
+int mode_1 = 5; // CN9 pin6 /Arduino 5 (PD5)
+int mode_2 = 10; // CN5 pin3 /Arduino 10 (PB2)
 int mode_3 = 3; // CN9 pin4 /Arduino 3 (= STCK)
 int mode_4 = 7; // CN9 pin8 /Arduino 7
 
-int STEP = 3; // DB STCK PWM1 (DIO 3) (CN9 pin4)
-int DIR = 7; // DB DIR1 (CN9 pin8)
+int STEP = 3; // DB STCK PWM1 (DIO 3) (CN9 pin4) (PD3)
+int DIR = 7; // DB DIR1 (CN9 pin8) (PD7)
 
 const int STBY=8;     // STBY = Standby\reset input. When forced low the device enters in low consumption mode: all motor-coils are non-current 
 
@@ -38,7 +47,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(mode_1,OUTPUT); 
   pinMode(mode_2,OUTPUT); 
-  pinMode(mode_3,OUTPUT); // == pinMode(STEP,OUTPUT); 
+  pinMode(mode_3,OUTPUT); // == pinMode(STEP,OUTPUT); (
   pinMode(mode_4,OUTPUT); // == pinMode(DIR,OUTPUT);
   pinMode(STBY, OUTPUT);  
 
@@ -80,24 +89,31 @@ void setup() {
 unsigned long time_res;
 unsigned long time_1;
 unsigned long time_2;
+unsigned long time_3;
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(DIR,HIGH); // + Direction
-  
-  for(int x = 0; x < 1*rot_step_ustep; x++) {  // 200 pulses one revolution
-//   time_1 = micros();   
-   digitalWrite(STEP,HIGH); 
-//   delayMicroseconds(int(step1_period/2)); 
-   delayMicroseconds(control_time); 
-   time_1 = micros();   
-   time_2 = micros();
-   
-   digitalWrite(STEP,LOW);   
-   delayMicroseconds(control_time); 
+
+  // number of rotation
+  for(int i = 0; i<10;i++)
+  {
+    ///// 1 rotation loop
+    PORTD = B10000000; // digital pin 7 HIGH
+    for(int x = 0; x < rot_step_ustep; x++) {  // 3 rotation
+      time_1 = micros(); // t1 = when it start rotation    
+      PORTD = B10001000; // digital pin 3 HIGH
+      while(time_2 - time_1 < control_time){
+        time_2 = micros();   
+      }
+      PORTD = B10000000; // digital pin 3 LOW
+      while(time_3 - time_2 < control_time){
+        time_3 = micros();   
+      }
+    }
+    
   }
-  time_res = time_2 - time_1;
-  Serial.println(time_res);
-  delay(1000); 
+  // rest
+  PORTD = B00000000; // digital pin 7 LOW 
+  delay(2000); 
   
 
 }
@@ -108,18 +124,18 @@ void setup_StepperMTDR(int STBY, int mode_1,int mode_2, int mode_3, int mode_4){
   delayMicroseconds(2); // at least 1us
   
 
-  digitalWrite(mode_1,HIGH); //
+  digitalWrite(mode_1,LOW); //
   digitalWrite(mode_2,HIGH); //
-  digitalWrite(mode_3,LOW); //
+  digitalWrite(mode_3,HIGH); //
   digitalWrite(mode_4,LOW); //
 
   digitalWrite(STBY,HIGH);
   delayMicroseconds(120); // at least 100us
 
-//  digitalWrite(mode_1,HIGH); //
-//  digitalWrite(mode_2,HIGH); //
-  digitalWrite(mode_3,HIGH); //  
-  digitalWrite(mode_4,LOW); //
+////  digitalWrite(mode_1,HIGH); //
+////  digitalWrite(mode_2,HIGH); //
+//  digitalWrite(mode_3,HIGH); //  
+//  digitalWrite(mode_4,LOW); //
 }
 
   
